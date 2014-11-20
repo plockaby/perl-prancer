98ce338194f1a46f502dc893f8d866cfbfbaf0ed
Paul Lockaby <paul@paullockaby.com>
2014-11-20 06:45:37 +0000

    removing tabs

--- examples/bar.pl
+++ examples/bar.pl
@@ -9,9 +9,9 @@ sub main {
     # figure out where exist to make finding config files possible
     my (undef, $root, undef) = File::Basename::fileparse($0);
 
-	# this just returns a prancer object so we can get access to configuration
-	# options and other awesome things like plugins.
-	my $app = Prancer->new("${root}/foobar.yml");
+    # this just returns a prancer object so we can get access to configuration
+    # options and other awesome things like plugins.
+    my $app = Prancer->new("${root}/foobar.yml");
 
     print "hello, goodbye. foo = " . $app->config->get('foo') . " or " . config->get('foo') . "\n";
 
